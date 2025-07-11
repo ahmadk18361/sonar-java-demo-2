fc77e42fbbb3d36df5146087dce374c3013e33fa
ahmadk18361 <ahmadk18361@gmail.com>
2025-07-11 17:20:30 -0400

    Update TomcatCVE2019_0232Example.java

--- src/main/java/com/example/TomcatCVE2019_0232Example.java
+++ src/main/java/com/example/TomcatCVE2019_0232Example.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class TomcatCVE2019_0232Example {
     public static void main(String[] args) throws IOException {
-        // REMEDIATED: Avoid using Runtime exec with untrusted input
-System.out.println("Listing directory contents..."); // Vulnerable: Untrusted input could be injected
+        String command = "cmd.exe /c dir";
+        Runtime.getRuntime().exec(command); // Vulnerable: Untrusted input could be injected
     }
 }
